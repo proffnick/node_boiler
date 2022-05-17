9377cb56e5fd3f26e243598cf0f2771824c69d1c
adimscredit@gmail.com <proffnick1@gmail.com>
2022-05-17 04:20:26 +0100

    my very first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # node_boiler
 The node js boiler plate
+
+# clone and run npm i
+simply clone the project and run npm to install packages
+
+# uses mongo db 
+You can change the DB connection to your own connection stcing
--- startup/routes.js
+++ startup/routes.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const errors = require('../middleware/error');
 const users = require('../routes/users');
+const listings = require('../routes/listings');
 const auth = require('../routes/auth');
 
 module.exports = function(app){ 
         // accept json request
         app.use(express.json());
         app.use('/api/users', users);
+        app.use('/api/listings', listings);
         app.use('/api/auth', auth);
         app.use(errors);
 }
