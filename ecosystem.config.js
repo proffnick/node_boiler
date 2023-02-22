d09a3036a893c30e25c18fd3fdab728ab1386b79
adimscredit@gmail.com <proffnick1@gmail.com>
2023-02-22 21:30:57 +0100

    update on 22 Feb 2023 9:30 pm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,23 +2,11 @@ module.exports = {
     apps : [{
       name   : "psa-logistics",
       script : "/home/ec2-user/apps/psa/index.js",
-      watch: ["/home/ec2-user/apps/psa/package.json", "/home/ec2-user/apps/psa/middleware", "/home/ec2-user/apps/psa/model", "/home/ec2-user/apps/psa/routes", "/home/ec2-user/apps/psa/startup", "/home/ec2-user/apps/psa/db", "/home/ec2-user/apps/psa/config", "/home/ec2-user/apps/psa/index.js"],
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      }
+      watch: ["/home/ec2-user/apps/psa/package.json", "/home/ec2-user/apps/psa/middleware", "/home/ec2-user/apps/psa/model", "/home/ec2-user/apps/psa/routes", "/home/ec2-user/apps/psa/startup", "/home/ec2-user/apps/psa/db", "/home/ec2-user/apps/psa/config", "/home/ec2-user/apps/psa/index.js"]
     }, 
     {
         name: "new_orders",
-        script: "/home/ec2-user/apps/psa/bin/new_orders.js",
-        env: {
-          NODE_ENV: "development",
-        },
-        env_production: {
-          NODE_ENV: "production",
-        }
+        script: "/home/ec2-user/apps/psa/bin/new_orders.js"
     }
     ]
   }
