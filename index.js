311d6988e7f3377ffb1c670b241262f16c8319d4
adimscredit@gmail.com <proffnick1@gmail.com>
2023-08-14 03:03:36 +0100

    updated on August 14th 2023 at 3:03 AM

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express   = require('express');
 const app       = express();
+const https = require('https');
+const fs = require('fs');
 
 require('./startup/logging')();
 require('./startup/routes')(app);
@@ -8,8 +10,14 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/app.psalogistics.ng/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/app.psalogistics.ng/fullchain.pem'),
+};
+
+const server = https.createServer(options, app);
 
 const port = process.env.PORT || 5555;
-app.listen(port, () =>{
+server.listen(port, () =>{
     console.log(`App Listening at port ${port}`);
 });
