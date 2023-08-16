f7bd28134408686a241dc42491c75186030753cd
adimscredit@gmail.com <proffnick1@gmail.com>
2023-08-16 01:44:07 +0100

    updated on 16th August 2023 @ 1:43 AM

--- routes/mails.js
+++ routes/mails.js
@@ -13,60 +13,74 @@ const template = "<div style='background-color:#f6efed; background-repeat:no-rep
 // async..await is not allowed in global scope, must use a wrapper
 const main = async (sender, receiver, subject, html, text) => {
 
-  const OAuth2Client = new google.auth.OAuth2(
-    config.get('gmailClientId'),
-    config.get('gmailClientSecret'),
-    config.get('gmailRedirectUrl'),
-    );
+    try {
+      const OAuth2Client = new google.auth.OAuth2(
+        config.get('gmailClientId'),
+        config.get('gmailClientSecret'),
+        config.get('gmailRedirectUrl'),
+        );
+    
+      OAuth2Client.setCredentials({ refresh_token: config.get('gmailRefreshToken') });
+      
+        // create reusable transporter object using the default SMTP transport
+        //type: "SMTP",
+        //host: "smtp.gmail.com",
+        //port: 465,//sudo ufw allow 587  sudo ufw delete allow from 587
+        //secure: true, // true for 465, false for other ports
+    
+        const accessToken = await OAuth2Client.getAccessToken();
+    
+        let transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            type: 'OAuth2',
+            user: 'psaapp1@gmail.com',
+            clientId: config.get('gmailClientId'),
+            clientSecret: config.get('gmailClientSecret'),
+            refreshToken: config.get('gmailRefreshToken'),
+            accessToken: accessToken?.res?.data?.access_token,
+            expires: 1658084390277
+          },
+        });
+      
+        // send mail with defined transport object
+        // 315274394082-l58ceoj4ngi9lnk7emnrbsait2dr46jo.apps.googleusercontent.com
+        //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
+        let info = await transporter.sendMail({
+          from: '"PSA" <'+sender+'>', // sender address
+          to: receiver, // list of receivers
+          subject: subject, // Subject line
+          text: text, // plain text body
+          html: html, // html body
+        });
+      
+        return ("Message sent: %s", info.messageId);
+    } catch (error) {
+      console.log(error, error?.message, " sending message ");
+    }
 
-  OAuth2Client.setCredentials({ refresh_token: config.get('gmailRefreshToken') });
-  
-    // create reusable transporter object using the default SMTP transport
-    //type: "SMTP",
-    //host: "smtp.gmail.com",
-    //port: 465,//sudo ufw allow 587  sudo ufw delete allow from 587
-    //secure: true, // true for 465, false for other ports
 
-    const accessToken = await OAuth2Client.getAccessToken();
-
-    let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        type: 'OAuth2',
-        user: 'psaapp1@gmail.com',
-        clientId: config.get('gmailClientId'),
-        clientSecret: config.get('gmailClientSecret'),
-        refreshToken: config.get('gmailRefreshToken'),
-        accessToken: accessToken?.res?.data?.access_token,
-        expires: 1658084390277
-      },
-    });
-  
-    // send mail with defined transport object
-    // 315274394082-l58ceoj4ngi9lnk7emnrbsait2dr46jo.apps.googleusercontent.com
-    //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
-    let info = await transporter.sendMail({
-      from: '"PSA" <'+sender+'>', // sender address
-      to: receiver, // list of receivers
-      subject: subject, // Subject line
-      text: text, // plain text body
-      html: html, // html body
-    });
-  
-    return ("Message sent: %s", info.messageId);
   }
 
 // adding a new genre
 router.post('/send', auth, async (req, res) => {
-    const sender        = 'psaapp1@gmail.com';
-    const receiver      = req.body.email;
-    const message       = req.body.message;
-    const subject       = req.body.subject;
-    const finalMessage  = template.replace('[TITLE]', subject).replace('[CONTENT]', message);
-    const sent          = await main(sender, receiver, subject, finalMessage, message).catch(error => console.log(error));
-    res.status(200).send(sent);
+    try {
+      const sender        = 'psaapp1@gmail.com';
+      const receiver      = req.body.email;
+      const message       = req.body.message;
+      const subject       = req.body.subject;
+      const finalMessage  = template.replace('[TITLE]', subject).replace('[CONTENT]', message);
+      const sent          = await main(sender, receiver, subject, finalMessage, message).catch(error => console.log(error));
+      console.log(sent, " sent message ");
+      res.status(200).send(sent);
+    } catch (error) {
+      console.log(error, error?.message, " Message ");
+      res.status(404).send({status: false, message: error?.message});
+    }
+    
 
   });
+
 router.post('/reset', async (req, res) => {
 
     const sender        = 'psaapp1@gmail.com';
